05ac0dfa6bee30867167dcbeaf67f37b119cc07b
CrazyGuy108 <taylor@hansenfamily.us>
2018-10-06 19:48:35 -0700

    Implement training script
    For now this just plays the AI against itself for one game.

--- scripts/train.ts
+++ scripts/train.ts
@@ -10,15 +10,37 @@ import { Parser } from "../src/parser/Parser";
 // @ts-ignore
 import Sim = require("./Pokemon-Showdown/sim");
 
+// main event loop
 const stream = new Sim.BattleStream();
 (async function()
 {
-    let output;
+    let output: string;
     while (output = await stream.read())
     {
-        logger.debug(`received: ${output}`);
+        console.log(`received: ${output}`);
+        parse(output);
+        // await keypress();
     }
 })();
+
+/** Waits for a keypress to continue. */
+async function keypress(): Promise<void>
+{
+    return new Promise<void>(resolve =>
+    {
+        process.stdin.once("data", resolve);
+    });
+}
+
+/**
+ * Writes a response to the stream.
+ * @param response Response to send.
+ */
+function writeStream(response: string): void
+{
+    console.log(`sent: ${response}`);
+    stream.write(response);
+}
 
 /**
  * Creates a function that sends the AI's response to the battle stream.
@@ -29,7 +51,7 @@ function createChoiceSender(playerId: PlayerID): ChoiceSender
 {
     return function(choice: Choice)
     {
-        stream.write(`${playerId} ${choice}`);
+        writeStream(`>${playerId} ${choice}`);
     };
 }
 
@@ -55,10 +77,67 @@ function initPlayer(id: PlayerID): Player
     const listener = parser.getListener("");
     const battle = new Battle(Network, id, /*saveAlways*/ false, listener,
         createChoiceSender(id));
-    stream.write(`>player ${id} {"name":"${id}"}`);
+    writeStream(`>player ${id} {"name":"${id}"}`);
     return {id, battle, parser};
 }
 
-stream.write(`>start {"formatid":"${Bot.format}"}`);
-const player1 = initPlayer("p1");
-const player2 = initPlayer("p2");
+writeStream(`>start {"formatid":"${Bot.format}"}`);
+const players: {[ID in PlayerID]: Player} =
+    {p1: initPlayer("p1"), p2: initPlayer("p2")};
+
+/**
+ * Parses messages from the BattleStream.
+ * @param data Message string data.
+ */
+function parse(data: string): void
+{
+    const lines = data.split("\n");
+    const [command] = data.split("\n", 1);
+    switch (command)
+    {
+        case "update":
+        {
+            parseUpdate(lines);
+            break;
+        }
+        case "sideupdate":
+        {
+            // send messages exclusively to this player
+            const id = lines[1] as PlayerID;
+            players[id].parser.parse(lines.slice(2).join("\n"));
+            break;
+        }
+        case "end":
+        {
+            // json logdata
+            const logData = JSON.parse(lines[1]);
+            break;
+        }
+    }
+}
+
+/**
+ * Parses an `update` stream message.
+ * @param lines Messages that need to be parsed.
+ */
+function parseUpdate(lines: string[]): void
+{
+    for (let i = 1; i < lines.length; ++i)
+    {
+        const line = lines[i];
+        if (line === "|split")
+        {
+            // different versions of the message are sent to different observers
+            // 1st: spectator, 2nd: p1, 3rd: p2, 4th: omniscient
+            players.p1.parser.parse(lines[i + 2]);
+            players.p2.parser.parse(lines[i + 3]);
+            i += 4;
+        }
+        else
+        {
+            // both players parse the message like normal
+            (Object.keys(players) as PlayerID[]).forEach(
+                id => players[id].parser.parse(line));
+        }
+    }
+}
