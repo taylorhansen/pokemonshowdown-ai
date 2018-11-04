23a7c9f83ccb4ef99fbae98e86b4c0704c16b4c6
CrazyGuy108 <taylor@hansenfamily.us>
2018-11-04 10:00:39 -0800

    Fix import error

--- test/bot/battle/MockBattle.ts
+++ test/bot/battle/MockBattle.ts
@@ -1,6 +1,7 @@
 import { Battle } from "../../../src/bot/battle/Battle";
 import { Choice } from "../../../src/bot/battle/Choice";
-import { BattleState, Side } from "../../../src/bot/battle/state/BattleState";
+import { BattleState } from "../../../src/bot/battle/state/BattleState";
+import { Side } from "../../../src/bot/battle/state/Side";
 import { PlayerID } from "../../../src/bot/messageData";
 
 /** Mocks the Battle class to get access to certain fields. */
