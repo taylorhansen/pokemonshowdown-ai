0d386e3c558098f6081a6cbb8907ef7492d39a4e
CrazyGuy108 <taylor@hansenfamily.us>
2019-02-04 20:31:38 -0800

    Add utility test

---
+++ test/bot/battle/state/utility.ts
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import "mocha";
+import { oneHot, tempStatusTurns } from
+    "../../../../src/bot/battle/state/utility";
+
+describe("utility", function()
+{
+    describe("oneHot", function()
+    {
+        it("Should encode class of values", function()
+        {
+            expect(oneHot(2, 3)).to.deep.equal([0, 0, 1]);
+        });
+    });
+
+    describe("tempStatusTurns", function()
+    {
+        it("Should return 0 if given 0", function()
+        {
+            expect(tempStatusTurns(0)).to.equal(0);
+        });
+
+        it("Should decrease status likelihood", function()
+        {
+            expect(tempStatusTurns(2)).to.equal(1 / 2);
+        });
+    });
+});
