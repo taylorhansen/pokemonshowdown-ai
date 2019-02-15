e8dfc88e946f52251a880ec0ff80f35c0bb5b5a7
CrazyGuy108 <taylor@hansenfamily.us>
2019-02-15 12:13:38 -0800

    Remove some unused code

--- src/bot/README (todo).md
+++
@@ -1,8 +0,0 @@
-* Add the line prefix (between the first two `|` characters) to the
-  eventPrefixes object.
-* Add the name of the event type (without any dashes) to the
-  battleEventTypesInternal dictionary.
-* Implement a BattleEvent-derived interface and add it to the BattleEvent
-  generic type.
-* See MessageParser for parsing details.
-* See BattleEventListener and EventProcessor for event handling details.
--- src/bot/helpers.ts
+++ src/bot/helpers.ts
@@ -1,18 +1,5 @@
 /** @file Contains useful helper types. */
 
-/** Type that can be null. */
-export type Nullable<T> = T | null;
-
-/** Object type where any field can be null. */
-export type ShallowNullable<T> =
-    {[N in keyof T]: Nullable<T[N]>};
-
-/** Object type where any field and fields of that field can be null. */
-export type DeepNullable<T> =
-{
-    [N in keyof T]: Nullable<T[N] extends object ? DeepNullable<T[N]> : T[N]>
-};
-
 /** Player ID in a battle. */
 export type PlayerID = "p1" | "p2";
 
