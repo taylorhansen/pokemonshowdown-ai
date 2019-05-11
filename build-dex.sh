602f52f2ef32921f020ffd029a3dbb4890d07d3a
CrazyGuy108 <taylor@hansenfamily.us>
2019-05-11 10:16:31 -0700

    Use locally installed tools

--- build-dex.sh
+++ build-dex.sh
@@ -3,11 +3,11 @@
 
 echo Building dex...
 dex_file=src/battle/dex/dex.ts
-ts-node scripts/build-dex.ts > $dex_file
+npx ts-node scripts/build-dex.ts > $dex_file
 
 # try to fix any style errors
 # this will also point out errors that must be fixed manually
-tslint --fix $dex_file
+npx tslint --fix $dex_file
 
 # tslint will make this script return an error code so override that
 exit 0
