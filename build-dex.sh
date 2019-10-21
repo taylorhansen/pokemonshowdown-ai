da94493549d516568b6c049b6e62fedb19e8720f
CrazyGuy108 <taylor@hansenfamily.us>
2019-10-21 11:27:28 -0700

    Silence dex tslint warnings

--- build-dex.sh
+++ build-dex.sh
@@ -5,9 +5,8 @@ echo Building dex...
 dex_file=src/battle/dex/dex.ts
 npx ts-node scripts/build-dex.ts > $dex_file
 
-# try to fix any style errors
-# this will also point out errors that must be fixed manually
-npx tslint --fix $dex_file
+# try to fix any style errors, ignoring unfixable ones
+npx tslint --fix $dex_file > /dev/null
 
 # tslint will make this script return an error code so override that
 exit 0
