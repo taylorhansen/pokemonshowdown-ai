c332488279ba5e14dd6ec1191a3eedea7aebd6c8
CrazyGuy108 <taylor@hansenfamily.us>
2019-12-31 20:20:46 -0800

    Scale down network for now
    Haven't decided on cloud hosting yet, so make the network smaller and
    faster to train for now so things can be tested.

--- scripts/train/model.ts
+++ scripts/train/model.ts
@@ -8,9 +8,7 @@ export function createModel(): tf.LayersModel
 {
     const model = tf.sequential();
     model.add(tf.layers.dense(
-        {inputShape: [sizeBattleState], units: 20000, activation: "elu"}));
-    model.add(tf.layers.dropout({rate: 0.3}));
-    model.add(tf.layers.dense({units: 1000, activation: "elu"}));
+        {inputShape: [sizeBattleState], units: 1000, activation: "elu"}));
     model.add(tf.layers.dropout({rate: 0.3}));
     model.add(tf.layers.dense(
         {units: intToChoice.length, activation: "linear"}));
