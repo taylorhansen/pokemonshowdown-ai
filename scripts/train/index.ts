51e8377f9dbabf0aa89ea2ca96798d05c2e5bef0
CrazyGuy108 <taylor@hansenfamily.us>
2020-01-22 15:59:41 -0800

    Change training parameters
    Increase games, gamma, batch size and memory size. The memory should be
    able to hold the equivalent of about 10-20 past games, while a batch
    goes over around half of a game.

--- scripts/train/index.ts
+++ scripts/train/index.ts
@@ -1,19 +1,4 @@
-/**
- * @file Plays the neural network against itself for several times before
- * training and evaluation.
- *
- * The algorithm is as follows:
- * 1. Construct a neural network.
- * 2. Play the network against itself, storing Experience objects during play to
- *    be used for learning later.
- * 3. After a number of games, train a copy of the neural network using all of
- *    the stored Experiences.
- * 4. Evaluate the newly trained network against the old one to see if the old
- *    one should be replaced on the next training cycle. This is done by playing
- *    some number of games and seeing if the new network generally beats the
- *    old one.
- * 5. Repeat steps 2-4 as desired.
- */
+/** @file Sets up a training session for the neural network. */
 import * as tf from "@tensorflow/tfjs-node";
 import { join } from "path";
 import { Network } from "../../src/ai/Network";
@@ -47,10 +32,10 @@ import { train } from "./train";
     await train(
     {
         model, saveUrl: modelUrl,
-        games: 5,
-        gamma: 0.8,
+        games: 25,
+        gamma: 0.95,
         explore: {start: 1, stop: 0.01, decay: 0.001},
-        batchSize: 16, memorySize: 256,
+        batchSize: 64, memorySize: 1024,
         logPath
     });
 })();
