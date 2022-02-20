16f9f875807fcb1c0caf829508a4e7a5233c98b3
taylorhansen <taylor@hansenfamily.us>
2022-02-19 16:41:12 -0800

    Clarify comment

--- src/psbot/handlers/battle/ai/helpers.ts
+++ src/psbot/handlers/battle/ai/helpers.ts
@@ -5,7 +5,8 @@
  * Randomly shuffles the given array according to their corresponding
  * probabilities.
  *
- * @param weights A probability distribution for each corresponding index.
+ * @param weights A probability distribution for each corresponding index. All
+ * weights must be positive, but they don't need to sum to 1.
  * @param arr Array to sort.
  */
 export function weightedShuffle<T>(weights: number[], arr: T[]): void {
@@ -16,6 +17,7 @@ export function weightedShuffle<T>(weights: number[], arr: T[]): void {
                 `(weights: ${weights.length}, arr: ${arr.length})`,
         );
     }
+    // Cumulative sum version of weights array.
     const cw = weights.map(
         (
             sum => (value: number) =>
@@ -27,19 +29,19 @@ export function weightedShuffle<T>(weights: number[], arr: T[]): void {
         // Get a random number between 0 and the sum of all the weights.
         // On the first iteration, this is between 0 and 1 approx.
         const rand = Math.random() * cw[cw.length - 1];
-        // Choose the first cumulative weight that is greater than this
-        // number, using a binary search since a cumulative sum array is
-        // always ordered.
+        // Choose the first cumulative weight that is greater than this number,
+        // using a binary search since a cumulative sum array is always ordered.
         // Higher weight values have a greater chance of being selected here.
         const j = bisectRight(cw, rand);
         // The index of that weight will correspond to the next element that
-        // will be added to the list.
+        // will be added to the result array.
         arr[i] = copy[j];
 
-        // Remove the selected weight value from all of the cumulative
-        // weights that had this value, and also their corresponding values.
+        // Remove the selected weight value from all of the cumulative weights
+        // that had this value, and also their corresponding values.
         // On the next iteration, the elements that were not selected will
-        // compete for the next spot on the list.
+        // compete for the next spot on the result array, still based on their
+        // corresponding weights.
         for (let k = j; k < cw.length; ++k) {
             cw[k] -= weights[j];
         }
@@ -50,8 +52,8 @@ export function weightedShuffle<T>(weights: number[], arr: T[]): void {
 }
 
 /**
- * Searches a sorted array `a` for an insertion index for the given number
- * `n`. If there are any duplicates, the right-most insertion index is chosen.
+ * Searches a sorted array `a` for an insertion index for the given number `n`.
+ * If there are any duplicates, the right-most insertion index is chosen.
  */
 export function bisectRight(
     a: readonly number[],
