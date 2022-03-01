8775145b65aeb9aa8a6e7ed5e850239566860870
taylorhansen <taylor@hansenfamily.us>
2022-02-28 22:50:31 -0800

    Specify opponents during training/eval games

--- src/train/play/playGames.ts
+++ src/train/play/playGames.ts
@@ -66,32 +66,37 @@ export async function playGames({
     rollout,
     getExpPath,
 }: PlayGamesArgs): Promise<number> {
-    const totalGames = opponents.reduce((n, op) => n + op.numGames, 0);
-    const prefixWidth = logger.prefix.length;
-    const postfixWidth =
-        " wlt=--".length + 3 * Math.ceil(Math.log10(totalGames));
-    const padding = 2;
-    const barWidth =
-        (process.stdout.columns || 80) - prefixWidth - postfixWidth - padding;
-    const progress = new ProgressBar(`${logger.prefix}:bar wlt=:wlt`, {
-        total: totalGames,
-        head: ">",
-        clear: true,
-        width: barWidth,
-    });
-    progress.render({wlt: "0-0-0"});
-    const progressLogFunc: LogFunc = msg => progress.interrupt(msg);
-    const progressLog = new Logger(
-        progressLogFunc,
-        progressLogFunc,
-        logger.prefix,
-        logger.postfix,
-    );
+    let numAExps = 0;
+    for (const opponent of opponents) {
+        const innerLog = logger.addPrefix(`Versus ${opponent.name}: `);
+        const totalGames = opponent.numGames;
+        const prefixWidth = innerLog.prefix.length;
+        const postfixWidth =
+            " wlt=--".length + 3 * Math.ceil(Math.log10(totalGames));
+        const padding = 2;
+        const barWidth =
+            (process.stdout.columns || 80) -
+            prefixWidth -
+            postfixWidth -
+            padding;
+        const progress = new ProgressBar(`${innerLog.prefix}:bar wlt=:wlt`, {
+            total: totalGames,
+            head: ">",
+            clear: true,
+            width: barWidth,
+        });
+        progress.render({wlt: "0-0-0"});
+        const progressLogFunc: LogFunc = msg => progress.interrupt(msg);
+        const progressLog = new Logger(
+            progressLogFunc,
+            progressLogFunc,
+            innerLog.prefix,
+            innerLog.postfix,
+        );
 
-    // Iterator-like stream for piping GamePoolArgs to the GamePool stream.
-    const poolArgs = stream.Readable.from(
-        (function* generateArgs() {
-            for (const opponent of opponents) {
+        // Iterator-like stream for piping GamePoolArgs to the GamePool stream.
+        const poolArgs = stream.Readable.from(
+            (function* generateArgs() {
                 for (let i = 0; i < opponent.numGames; ++i) {
                     const gameLogPath =
                         logPath &&
@@ -106,61 +111,59 @@ export async function playGames({
                     };
                     yield args;
                 }
-            }
-        })(),
-        {objectMode: true},
-    );
-
-    // Stream for summarizing the game results.
-    let numAExps = 0;
-    let wins = 0;
-    let losses = 0;
-    let ties = 0;
-    const processResults = new stream.Writable({
-        objectMode: true,
-        write(
-            result: GamePoolResult,
-            encoding: BufferEncoding,
-            callback: stream.TransformCallback,
-        ): void {
-            numAExps += result.numAExps;
-
-            if (result.err) {
-                progressLog.error(
-                    `Game ${result.id} threw an error: ` +
-                        `${result.err.stack ?? result.err.toString()}`,
-                );
-            }
-
-            if (result.winner === 0) {
-                ++wins;
-            } else if (result.winner === 1) {
-                ++losses;
-            } else {
-                ++ties;
-            }
-
-            progress.tick({wlt: `${wins}-${losses}-${ties}`});
-
-            callback();
-        },
-    });
-
-    // TODO: Move pool outside for reuse?
-    const pool = new GamePool(numThreads, getExpPath);
-    try {
-        await stream.promises.pipeline(
-            poolArgs,
-            new GamePoolStream(pool),
-            processResults,
+            })(),
+            {objectMode: true},
         );
-    } finally {
-        await pool.close();
-    }
 
-    progress.terminate();
-    // TODO: Also display separate records for each opponent.
-    logger.debug(`Record: ${wins}-${losses}-${ties}`);
+        // Stream for summarizing the game results.
+        let wins = 0;
+        let losses = 0;
+        let ties = 0;
+        const processResults = new stream.Writable({
+            objectMode: true,
+            write(
+                result: GamePoolResult,
+                encoding: BufferEncoding,
+                callback: stream.TransformCallback,
+            ): void {
+                if (result.err) {
+                    progressLog.error(
+                        `Game ${result.id} threw an error: ` +
+                            `${result.err.stack ?? result.err.toString()}`,
+                    );
+                }
+
+                numAExps += result.numAExps;
+
+                if (result.winner === 0) {
+                    ++wins;
+                } else if (result.winner === 1) {
+                    ++losses;
+                } else {
+                    ++ties;
+                }
+
+                progress.tick({wlt: `${wins}-${losses}-${ties}`});
+
+                callback();
+            },
+        });
+
+        // TODO: Move pool outside for reuse?
+        const pool = new GamePool(numThreads, getExpPath);
+        try {
+            await stream.promises.pipeline(
+                poolArgs,
+                new GamePoolStream(pool),
+                processResults,
+            );
+        } finally {
+            await pool.close();
+        }
+
+        progress.terminate();
+        innerLog.debug(`Record: ${wins}-${losses}-${ties}`);
+    }
 
     return numAExps;
 }
