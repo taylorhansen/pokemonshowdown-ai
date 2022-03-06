78821a9b583a39124350c70c61b48f80988f7edc
taylorhansen <taylor@hansenfamily.us>
2022-03-05 18:18:36 -0800

    Add multiple tfrecord encode/decode test

--- src/train/tfrecord/encode_decode.test.ts
+++ src/train/tfrecord/encode_decode.test.ts
@@ -8,44 +8,61 @@ import {AExpEncoder} from "./encoder";
 
 export const test = () =>
     describe("AExpEncoder + AExpDecoder", function () {
-        it("Should encode and decode successfully", async function () {
-            const aexp: AugmentedExperience = {
-                action: 0,
-                advantage: 2,
-                probs: new Float32Array([0.1, 0.2, 0.3, 0.4]),
-                returns: 1,
-                state: flattenedInputShapes.map(
-                    length =>
-                        new Float32Array(Array.from({length}, (_, i) => i)),
-                ),
-                value: -0.5,
-            };
+        const testAexp1: AugmentedExperience = {
+            action: 0,
+            advantage: -1,
+            probs: new Float32Array([0.1, 0.2, 0.3, 0.4]),
+            returns: 1,
+            state: flattenedInputShapes.map(
+                length => new Float32Array(Array.from({length}, (_, i) => i)),
+            ),
+            value: -0.5,
+        };
 
-            const inputStream = stream.Readable.from(
-                (function* () {
-                    yield aexp;
-                })(),
-            );
-            const encoder = new AExpEncoder();
-            const decoder = new AExpDecoder();
-            const outputStream = new stream.Writable({
-                objectMode: true,
-                write(chunk: AugmentedExperience, encoding, callback) {
-                    try {
-                        expect(chunk).to.deep.equal(aexp);
-                    } catch (e) {
-                        callback(e as Error);
-                        return;
-                    }
-                    callback();
-                },
-            });
-
+        it("Should encode/decode one aexp", async function () {
+            const aexps: AugmentedExperience[] = [];
             await stream.promises.pipeline(
-                inputStream,
-                encoder,
-                decoder,
-                outputStream,
+                [testAexp1],
+                new AExpEncoder(),
+                new AExpDecoder(),
+                async function (source) {
+                    for await (const chunk of source) {
+                        aexps.push(chunk as AugmentedExperience);
+                    }
+                },
             );
+            expect(aexps).to.have.lengthOf(1);
+            expect(aexps[0]).to.deep.equal(testAexp1);
+        });
+
+        const testAexp2: AugmentedExperience = {
+            action: 1,
+            advantage: 0.5,
+            probs: new Float32Array([0.4, 0.3, 0.3]),
+            returns: -0.25,
+            state: flattenedInputShapes.map(
+                length =>
+                    new Float32Array(
+                        Array.from({length}, (_, i) => -1 / (i + 1)),
+                    ),
+            ),
+            value: 0.75,
+        };
+
+        it("Should encode/decode multiple aexps", async function () {
+            const aexps: AugmentedExperience[] = [];
+            await stream.promises.pipeline(
+                [testAexp1, testAexp2],
+                new AExpEncoder(),
+                new AExpDecoder(),
+                async function (source) {
+                    for await (const chunk of source) {
+                        aexps.push(chunk as AugmentedExperience);
+                    }
+                },
+            );
+            expect(aexps).to.have.lengthOf(2);
+            expect(aexps[0]).to.deep.equal(testAexp1);
+            expect(aexps[1]).to.deep.equal(testAexp2);
         });
     });
