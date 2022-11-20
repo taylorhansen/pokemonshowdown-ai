8129d1c2cb0919898267220758493cb7a28f1fc7
taylorhansen <taylor@hansenfamily.us>
2022-11-19 19:06:13 -0800

    Simplify ensureDir

--- src/util/paths/ensureDir.ts
+++ src/util/paths/ensureDir.ts
@@ -2,25 +2,14 @@ import * as fs from "fs";
 
 /** Ensures that a folder exists, creating intermediate folders if needed. */
 export async function ensureDir(path: fs.PathLike): Promise<void> {
-    if (!(await isDir(path))) {
-        return await mkdirRecursive(path);
-    }
-}
-
-/** Checks if given path is an existing directory. */
-async function isDir(url: fs.PathLike): Promise<boolean> {
-    let stat: fs.Stats;
+    let isDirectory: boolean;
     try {
-        stat = await fs.promises.stat(url);
+        const stat = await fs.promises.stat(path);
+        isDirectory = stat.isDirectory();
     } catch (e) {
-        return false;
+        isDirectory = false;
     }
-    return stat.isDirectory();
-}
-
-/** Async recursive mkdir, similar to `mkdir -p`. */
-async function mkdirRecursive(path: fs.PathLike): Promise<void> {
-    return await new Promise((res, rej) => {
-        fs.mkdir(path, {recursive: true}, err => (err ? rej(err) : res()));
-    });
+    if (!isDirectory) {
+        await fs.promises.mkdir(path, {recursive: true});
+    }
 }
